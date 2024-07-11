abc79305b44d92e6d0c744f97a59fefa9fb5ec45
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-11 16:25:41 +0200

    [UPD] There are now some instruction into js file

---
+++ script/script.js
@@ -0,0 +1,11 @@
+/*
+
+ISTRUZIONI:
+1. creare struttura nel dom 
+2. rimuovere elementi statici dalla struttura nel dom
+3. inserirli tramite lo script (gli elementi saranno oggetto di un array).
+    >>l'immagine attiva mostrerà un titolo e un testo 
+4. creare una funzione che permetta la navigazione (attraverso dei bottoni) dei contenuti nel carosello
+5. rendere la navigazione "infinita": dunque una volta raggiunta l'ultima immagine della lista si riparte dalla prima.
+*/
+
